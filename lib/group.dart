2e7b54b714d3b70feced90106b3a71d265821a77
AmitKazado <AmitKazado@gmail.com>
2021-05-06 17:33:03 +0300

    Initial commit

---
+++ lib/group.dart
@@ -0,0 +1,8 @@
+import 'package:social_trip/user.dart';
+
+class Group{
+
+  User admin;
+  List<User> group;
+
+}
---
+++ lib/user.dart
@@ -0,0 +1,13 @@
+class User{
+
+  String name;
+  String email;
+
+  bool isAdmin = false;
+
+  User(String _name, String _email){
+    this.name = _name;
+    this.email = _email;
+  }
+
+}
